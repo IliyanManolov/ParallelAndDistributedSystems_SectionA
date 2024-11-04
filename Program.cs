8fddefac229087c31201fa41e0f1f1f434207a8a
Iliyan <.>
2024-11-04 21:26:58 +0200

    Setup w/ random generator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ParallelAndDistributedSystems_SectionA;
+
 Console.WriteLine("Hello, World!");
+
+// Basic generator for random int values in an array, added as a separate class to keep the main clean
+var generator = new RandomGenerator(0, 200);
+
+const int elements = 100000;
+
+var unsortedArray = generator.Generate(elements);
+
---
+++ RandomGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace ParallelAndDistributedSystems_SectionA;
+
+public class RandomGenerator
+{
+    private readonly int _minValue;
+    private readonly int _maxValue;
+
+    public RandomGenerator(int minValue, int maxValue)
+    {
+        _minValue = minValue;
+        _maxValue = maxValue;
+    }
+
+    public int[] Generate(int size)
+    {
+        var result = new int[size];
+
+        var random = new Random();
+        for(int i = 0; i < size; i++)
+        {
+            result[i] = random.Next(_minValue, _maxValue);
+        }
+
+        return result;
+    }
+}
