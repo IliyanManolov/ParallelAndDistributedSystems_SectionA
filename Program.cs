690d66b5ca0dc53bfade25905427ae953df0d54d
Iliyan <.>
2024-11-04 21:42:46 +0200

    Basic plan

--- Program.cs
+++ Program.cs
@@ -9,3 +9,15 @@ const int elements = 100000;
 
 var unsortedArray = generator.Generate(elements);
 
+
+void ParallelBubbleSort(int[] arr, int maxThreads)
+{
+    // PLAN:
+    // - split array into different groups/chunks (number of threads !)
+    //      - wont need communication
+    // - bubble sort each chunk
+    // - merge all chunks when all tasks are done
+
+
+    // TODO: discuss if splitting the array still counts as bubble sort and not bucket sort
+}
