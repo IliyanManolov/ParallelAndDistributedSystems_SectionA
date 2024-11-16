3dc20c44bf423a1666a355ffede0b1e9bd3e9c74
Iliyan <.>
2024-11-16 22:15:47 +0200

    Misc fixes for Task 1 (fixed issue with last item not being ordered always)

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ using System.Diagnostics;
 // Basic generator for random int values in an array, added as a separate class to keep the main clean
 
 const int elements = 100_000;
-const int maxThreads = 16;
+const int maxThreads = 6;
 
 // Uncomment to test Task 1 (Parralel bubble sort)
-//var generator = new RandomGenerator(0, 100_000);
-//var unsortedArray = generator.Generate(elements);
-//await ParallelBubbleSort(unsortedArray, maxThreads);
+var generator = new RandomGenerator(0, 100_000);
+var unsortedArray = generator.Generate(elements);
+await ParallelBubbleSort(unsortedArray, maxThreads);
 
 //// Uncomment to test Task 2 (Finding tools)
 //var toolGenerator = new RandomToolGenerator();
@@ -18,18 +18,10 @@ const int maxThreads = 16;
 
 Console.ReadLine();
 
-// TODO: redo tests after adding merging & migrate to .MD
-// THREADS - STOPWATCH TIME
-// 2 - 5,4604041
-// 3 - 2,3955435
-// 4 - 1,3065858
-// 6 - 0,5995377
-
-// 16 (PC max) -  0,1688517
 async Task ParallelBubbleSort(int[] arr, int maxThreads)
 {
     var watch = new Stopwatch();
-    int chunkSize = arr.Length / maxThreads;
+    int chunkSize = (int)Math.Ceiling((double)arr.Length / maxThreads);
 
     var tasks = new List<Task>();
 
@@ -39,16 +31,15 @@ async Task ParallelBubbleSort(int[] arr, int maxThreads)
     {
         int currentStart = i * chunkSize;
 
-        int currentLimit = (i == maxThreads - 1) ? arr.Length - 1 : (currentStart + chunkSize - 1);
+        var currentLimit = Math.Min(currentStart + chunkSize - 1, arr.Length - 1);
 
         tasks.Add(Task.Run(() => BubbleSort(arr, currentStart, currentLimit)));
     }
 
     await Task.WhenAll(tasks);
-    
 
     // Use the max threads since each thread creates 1 chunk
-    for (int i = 1; i < maxThreads; i++)
+    for (int i = 0; i < maxThreads; i++)
     {
         var start = 0;
         var middle = i * chunkSize;
@@ -61,13 +52,13 @@ async Task ParallelBubbleSort(int[] arr, int maxThreads)
     Console.WriteLine($"Finished execution with time '{watch.Elapsed.TotalSeconds}' seconds");
 
     // For basic debugging
-    //for (int i = 0; i < arr.Length - 1; i++)
-    //{
-    //    if (arr[i] > arr[i + 1])
-    //    {
-    //        Console.WriteLine($"UNSORTED AT INDEX {i} - value {arr[i]} > {arr[i + 1]}");
-    //    }
-    //}
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            Console.WriteLine($"UNSORTED AT INDEX {i} - value {arr[i]} > {arr[i + 1]}");
+        }
+    }
 
     // Basic in-place merge
     // Intentionally not using temporary array and cloning due to slightly better performance because of smaller sizes
@@ -84,19 +75,18 @@ async Task ParallelBubbleSort(int[] arr, int maxThreads)
             }
             else
             {
-                int value = array[j];
-                int index = j;
+                var currentValue = array[j];
+                var currentIndex = j;
 
-                while (index != i)
+                while (currentIndex != i)
                 {
-                    array[index] = array[index - 1];
-                    index--;
+                    array[currentIndex] = array[currentIndex - 1];
+                    currentIndex--;
                 }
-                array[i] = value;
+                array[i] = currentValue;
 
                 i++;
                 j++;
-                middle++;
             }
         }
     }
--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
 ﻿
-# Table is not mandatory, check why this is not working in VS Code
+Both tasks' execution times were tested with arrays of 100 000 elements as per the requirements listed in the Module book
 
 ## Task 1 Results
-| Threads count | Execution time in seconds [s,fffffff] |
-|				|							|
-| 2	| ... |
-| 3	| ... |
-| 4	| ... |
-| 6 | ... |
+
+Thread count : Execution time in seconds using [s,fffffff] format
+
+- 2 : 7,2684374
+- 3 : 4,8035566
+- 4 : 3,9155506
+- 6 : 3,4843255
 
 
 ## Task 2 Results
